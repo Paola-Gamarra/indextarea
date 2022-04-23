d03eaa9d270ef87494c01ba57a3b729117b99551
Paola-Gamarra <103967979+Paola-Gamarra@users.noreply.github.com>
2022-04-23 02:13:58 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log("Hola Mundo");
+console.log("hacer una torta");
+console.log("poner todos los ingredientes");
+console.log("cocinarlos");
+console.log("salir de casa");
+console.log("dirigirse al supermercado");
+console.log("facebbook: la vida es una");
+console.log("asi que a disfrutarla");
+console.log("tomar un rico cafe por las"):
+console.log("mañanas, es un plus de energia");
